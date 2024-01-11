0dcb2a9e1b34692c815cfe540ceaf639a10edf7f
Joshua Ilagan <thegodlyh@icloud.com>
2024-01-11 11:10:57 -0600

    added first view

---
+++ StudentView.swift
@@ -0,0 +1,20 @@
+//
+//  StudentView.swift
+//  Question of the Day
+//
+//  Created by Joshua S. Ilagan on 1/11/24.
+//
+
+import SwiftUI
+
+struct StudentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StudentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StudentView()
+    }
+}
---
+++ TeacherView.swift
@@ -0,0 +1,20 @@
+//
+//  TeacherView.swift
+//  Question of the Day
+//
+//  Created by Joshua S. Ilagan on 1/11/24.
+//
+
+import SwiftUI
+
+struct TeacherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeacherView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeacherView()
+    }
+}
