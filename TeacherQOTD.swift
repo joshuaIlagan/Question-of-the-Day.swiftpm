b28aa31cc9e3a4c46b53b35cb64cb3de2646e248
Joshua Ilagan <thegodlyh@icloud.com>
2024-02-07 11:09:16 -0600

    working on sign in

---
+++ TeacherQOTD.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherQOTD.swift
+//  Question of the Day
+//
+//  Created by Joshua S. Ilagan on 2/5/24.
+//
+
+import SwiftUI
+
+struct TeacherQOTD: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherQOTD()
+}
