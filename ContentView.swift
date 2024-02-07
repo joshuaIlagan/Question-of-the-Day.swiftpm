eb113e6982eb1b7eb57c013aa534e124c367783a
Ryan Rasavong <ryanrasavong@icloud.com>
2024-02-07 11:09:34 -0600

    working on sign in

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textInput: String = "Enter Question"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    TextField("Enter your text here", text: $textInput)
+ .padding()
+.textFieldStyle(RoundedBorderTextFieldStyle())
+
+.padding()
+
+            
+Text("Solve, \(textInput)")
+  .padding()
+            
         }
+        
+  .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
